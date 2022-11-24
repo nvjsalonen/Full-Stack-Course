e6740da224dae3779715de46addfc1195650cf18
nvjsalonen <nikolas.salonen@hotmail.com>
2022-11-24 22:21:23 +0200

    Modified App.js content component and created part component

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -1,3 +1,11 @@
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
 const Header = (props) => {  
   return (
     <div>
@@ -6,10 +14,21 @@ const Header = (props) => {
     )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
     <p> {props.part} {props.exercise}</p>
+    </>
+  )
+}
+
+
+const Content = () => {
+  return (
+    <>
+    <Part part={part1} exercise={exercises1}/>
+    <Part part={part2} exercise={exercises2}/>
+    <Part part={part3} exercise={exercises3}/>
     </>
   )
 }
@@ -21,20 +40,12 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
 
   return (
     <div>
       <Header name={course}/>
-      <Content part={part1} exercise={exercises1}/>
-      <Content part={part2} exercise={exercises2}/>
-      <Content part={part3} exercise={exercises3}/>
+      <Content/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
